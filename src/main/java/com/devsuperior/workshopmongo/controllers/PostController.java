0b6b3021592c634af8f1bbe5aface09140053b54
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-03-12 09:56:22 -0300

    GET post by id

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,9 +1,15 @@
 package com.devsuperior.workshopmongo.controllers;
 
+import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -12,13 +18,14 @@ public class PostController {
 	@Autowired
 	private PostService service;
 
-	/*
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
-		PostDTO dto = service.findById(id);
-		return ResponseEntity.ok(dto);
+	public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
+		return service.findById(id)
+				.map(postDto -> ResponseEntity.ok().body(postDto));
 	}
-	
+
+	/*
+
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
 		text = URL.decodeParam(text);
